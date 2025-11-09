4a1d1458112d61ff0b22823f7c6bb5522b667bf4
johan69 <johanorozco496@gmail.com>
2025-11-09 07:45:45 -0500

    archivo consumo simple api get php

---
+++ consumo.php
@@ -0,0 +1,18 @@
+<?php
+
+//echo "consumiremos los datos de una api usando codigo php nayivo y vamos a usar reglas de lectura de datata";
+
+
+// url de la data a consumir
+$url = "https://api.escuelajs.co/api/v1/users";
+
+// lectura de la api
+$Json = file_get_contents($url);
+
+// decode es el meto que de codifica el json de la api
+$data = json_decode($Json,true);
+ 
+print_r($data[0]) ;
+
+
+?>
